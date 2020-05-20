234f85b5732f6ce8258170ca33406e21ba2c8198
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2020-05-19 23:10:13 -0300

    settings configurations

--- skola/settings.py
+++ skola/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'skola.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
